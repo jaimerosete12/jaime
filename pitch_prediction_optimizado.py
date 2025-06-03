1749c74ee74de5274e4907fad5ce6398d1e835ba
jaimerosete12 <jaimerosete66@gmail.com>
2025-06-03 08:06:23 -0600

    Update pitch_prediction_optimizado.py

--- pitch_prediction_optimizado.py
+++ pitch_prediction_optimizado.py
@@ -69,7 +69,7 @@ def train_model(df, casino_line):
     X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
     if len(set(y_train)) < 2:
         raise ValueError("y_train contiene solo una clase. No se puede entrenar el modelo.")
-    model = XGBClassifier(use_label_encoder=False, eval_metric='logloss', objective='multi:softprob')
+    model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
     model.fit(X_train, y_train)
     model.label_encoder_ = le  # guardar para decodificar luego
     acc = accuracy_score(y_test, model.predict(X_test))
